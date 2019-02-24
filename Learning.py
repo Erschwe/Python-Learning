cd30967f1c26b2f670a3d18e94652cbee1c89bdd
Evan Schwenk <EvanRSchwenk@gmail.com>
2019-02-23 21:09:16 -0600

    OwO What's this?

---
+++ Learning.py
@@ -0,0 +1,7 @@
+# This is a learning environment for python
+
+
+#oh no i dropped my marbles ................................
+#........    ..................   ..................  ....
+
+# Please excuse the mess OwO
