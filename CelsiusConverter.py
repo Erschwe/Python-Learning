17befa08152a4f17d19e060301ce65dce7dd1611
Evan Schwenk <EvanRSchwenk@gmail.com>
2019-02-28 13:45:38 -0600

    Created Calculator with own method

--- CelsiusConverter.py
+++ CelsiusConverter.py
@@ -1,6 +1,9 @@
 
 Celsius = int(input("Enter a temperature to be converted to Fahrenheit: "))
 
-Fahrenheit = 9.0/5.0 * Celsius + 32
+#Fahrenheit = 9.0/5.0 * Celsius + 32
 
-print ("Temperature:", Celsius, "Celsius =", Fahrenheit, " F")
+#print ("Temperature:", Celsius, "Celsius =", Fahrenheit, " F")
+
+far = (Celsius * (9.0/5.0)) + 32
+print (far)
